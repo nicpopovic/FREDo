a46ae387fc39237ca034e7724afb9bc197e4412c
Nicholas Popovic <nicpopovic@me.com>
2022-10-18 14:11:33 +0200

    Update export_test_episodes.py
    corrected dataset for crossdomain episodes

--- export_test_episodes.py
+++ export_test_episodes.py
@@ -6,9 +6,9 @@ indomain_test_episodes_1 = parse_episodes_from_index("data/test_docred.json", "d
 
 indomain_test_episodes_3 = parse_episodes_from_index("data/test_docred.json", "data/test_in_domain_3_doc_indices.json", tokenizer=None, markers=False, cache=None, no_processing=True)
 
-crossdomain_test_episodes_1 = parse_episodes_from_index("data/test_docred.json", "data/test_cross_domain_1_doc_indices.json", tokenizer=None, markers=False, cache=None, no_processing=True)
+crossdomain_test_episodes_1 = parse_episodes_from_index("data/test_scierc.json", "data/test_cross_domain_1_doc_indices.json", tokenizer=None, markers=False, cache=None, no_processing=True)
 
-crossdomain_test_episodes_3 = parse_episodes_from_index("data/test_docred.json", "data/test_cross_domain_3_doc_indices.json", tokenizer=None, markers=False, cache=None, no_processing=True)
+crossdomain_test_episodes_3 = parse_episodes_from_index("data/test_scierc.json", "data/test_cross_domain_3_doc_indices.json", tokenizer=None, markers=False, cache=None, no_processing=True)
 
 with open('test_in_domain_1_doc.json', 'w') as fout:
     json.dump(indomain_test_episodes_1, fout)
